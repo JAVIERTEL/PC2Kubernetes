12d9dc39572b220741c8fdf96353ecbf159b5908
Javier González Pérez <125403900+JAVIERTEL@users.noreply.github.com>
2024-01-22 18:25:12 +0000

    Update comandos3.py

--- comandos3.py
+++ comandos3.py
@@ -5,18 +5,18 @@ def run_command(command):
     subprocess.call(command, shell=True)
 
 
-# Iniciamos un cluster de Kubernetes con 5 nodos
+# Iniciar cluster 
 run_command("minikube start --driver=docker -p pc2 --nodes 5")
 
-# Cambiamos al perfil que acabamos de crear
+# Cambiamos al perfil pc2
 run_command("minikube profile pc2")
 
-# Desplegamos los servicios y deployments
+# Desplegamos la aplicación
 run_command("minikube kubectl apply -- -f .")
 
-# Esperamos hasta que todos los pods se hayan iniciado
+# Esperamos a que los nods se inicien
 while (str(subprocess.check_output(["minikube kubectl get pods"], shell=True)).count("Running") < 9):
     time.sleep(1)
 
-# Abrimos la web de product-page
+# Abrimos la web 
 run_command("minikube service product-page")
